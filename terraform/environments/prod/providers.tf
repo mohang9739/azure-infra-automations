5ca7339ee31c381b83b166819113ca9312a665dd
Mohan G <mohang@Mohans-MacBook-Neo.local>
2026-08-02 16:56:47 +0530

    feat: add providers.tf with OIDC backend

---
+++ terraform/environments/prod/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "stterramohan001"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
